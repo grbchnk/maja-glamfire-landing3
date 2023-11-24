e9ec52c52a22db5f6518bed21f4283ff6ca2221a
Stas Gorbachenko <stsrnbw@gmail.com>
2023-11-25 02:33:49 +0300

    fix: video

--- scripts/videoPlays.js
+++ scripts/videoPlays.js
@@ -1,11 +1,9 @@
-const videoWrap = document.querySelector(".video-wrapper")
-
-function playVideo(videoWrap) {
-  const video = videoWrap.querySelector("video")
+function playVideo() {
+  const video = document.querySelector("video")
 
   if (!video) return
 
   video.play()
 }
 
-playVideo(videoWrap)
+playVideo()
